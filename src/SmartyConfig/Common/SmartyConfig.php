f2afa2dc7cadef55da64da91537ad8ee78d4c845
zhangchangyuan <1226852758@qq.com>
2022-07-22 15:21:31 +0800

    #203800-msg-依据checklist，完成信用武清网站功能开发、部署、测试

--- src/SmartyConfig/Common/SmartyConfig.php
+++ src/SmartyConfig/Common/SmartyConfig.php
@@ -6,7 +6,7 @@ class SmartyConfig
     public static function configDir()
     {
         $dirArray = array(
-            S_ROOT.'vendor/qixinyun/wuqing-portal-config/src/SmartyConfig/Wc',
+            S_ROOT.'vendor/qixinyun/wuqing-portal-config/src/SmartyConfig/Wq',
             S_ROOT.'vendor/qixinyun/wuqing-portal-config/src/SmartyConfig/Common',
             S_ROOT.'vendor/qixinyun/wuqing-portal-config/src/SmartyConfig/',
         );
